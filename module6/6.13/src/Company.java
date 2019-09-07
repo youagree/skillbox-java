3a45a3b1a0ba522866227677b5e842e837d370ef
Aleksey_Vasin <aevasin@vtb.ru>
2019-09-07 13:39:26 +0300

    after fix

--- module6/6.13/src/Company.java
+++ module6/6.13/src/Company.java
@@ -1,2 +1,51 @@
-public class Company {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Company extends Salary{
+
+    private List<Salary> list = new ArrayList<>();
+
+    public List<Salary> getList() {
+        return list;
+    }
+
+    public void addEmployee (int count){
+        for (int i = 0; i < count; i++) {
+            switch (RandomData.randomEmployee()) {
+                case 1:
+                    list.add(new OperationsOfficer());
+                    break;
+                case 2:
+                    list.add(new Manager());
+                    break;
+                case 3:
+                    list.add(new ManagerTop());
+                    break;
+            }
+        }
+    }
+
+    public List<Salary> removeTenPercentEmpl(int percent){
+        percent = (int)(list.size()*(percent/100.0f));
+        for (int i = 0; i < percent; i++){
+            list.remove(RandomData.random());
+        }
+        return list;
+    }
+
+    public void topFiveSalary(){
+        Collections.sort(list);
+        System.out.println("5 самых больших зарплат - ");
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(list.get(i).title + "-" + list.get(i).getMonthSalary());
+        }
+    }
+
+    public void printEmployee () {
+        list.forEach((employee) -> {
+            System.out.println(employee.getTitle() + "-" + employee.getMonthSalary());
+       });
+    }
+
 }
--- module6/6.13/src/Loader.java
+++ module6/6.13/src/Loader.java
@@ -1,64 +1,21 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 
 public class Loader {
 
-    private static final int TEN_PERCENT = (int)(270*(10.0f/100.0f));
-
     public static void main(String[] args) {
 
-        ArrayList<Salary> allEmployee = new ArrayList<>();
+    Company google = new Company();
 
-        for (int i = 0; i < 270; i++) {
-            switch (RandomData.randomEmployee()) {
-                case 1:
-                    allEmployee.add(new OperationsOfficer());
-                    break;
-                case 2:
-                    allEmployee.add(new Manager());
-                    break;
-                case 3:
-                    allEmployee.add(new ManagerTop());
-                    break;
-            }
-        }
+    google.addEmployee(300);
+    google.topFiveSalary();
+    System.out.println(Salary.getIncomeAll());
 
-        Collections.sort(allEmployee);
-//        System.out.println("Все сотрудники, всего " + allEmployee.size() + " человек.");
-//        System.out.println("Все менеджеры принесли " + Salary.getIncomeAll() + " руб.");
-//        String premium = Salary.getIncomeAll() > 10000000 ? "есть" : "не будет";
-//        System.out.println("Премия топ manager " + premium);
-//        allEmployee.forEach((employee) -> {
-//            System.out.println(employee.getTitle() + "-" + employee.getMonthSalary());
-//        });
+    google.removeTenPercentEmpl(10);
+    System.out.println(Salary.getIncomeAll());
 
-
-        topFiveSalary(allEmployee);
-        removeTenPercentEmpl(allEmployee);
-        topFiveSalary(allEmployee);
-
-//        System.out.println("5 самых маленьких зарплат - ");
-//        for (int i = 269; i >= 264; i--) {
-//
-//            System.out.println(allEmployee.get(i).title + "-" + allEmployee.get(i).getMonthSalary());
-//        }
+    Company microsoft = new Company();
+    microsoft.addEmployee(10);
+    microsoft.topFiveSalary();
     }
 
-
-    public  static List<Salary> removeTenPercentEmpl(List<Salary> list){
-        for (int i = 0; i < TEN_PERCENT; i++){
-            list.remove(RandomData.random());
-            System.out.println(RandomData.random());
-        }
-        return list;
-    }
-
-    public  static List<Salary> topFiveSalary(List<Salary> list){
-        System.out.println("5 самых больших зарплат - ");
-        for (int i = 0; i <= 5; i++) {
-            System.out.println(list.get(i).title + "-" + list.get(i).getMonthSalary());
-        }
-        return list;
-    }
 }

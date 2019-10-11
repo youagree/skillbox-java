8a3a76c1e533c1bfd54f63a8d47596078a22ed49
Aleksey Vasin <Aleksey_Vasin@epam.com>
2019-10-11 14:25:19 +0300

    restructuring 9.14

--- module9/9.14/src/main/java/Loader.java
+++ module9/9.14/src/main/java/Loader.java
@@ -1,38 +1,60 @@
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.Line;
+import dto.Metro;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 
 public class Loader {
 
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private static String fileName = Paths.get("src", "main", "resources", "underground.json").toString();
+    private static JSONParser jsonParser = new JSONParser();
+    private static Metro metro;
+    private static Parser parser = new Parser();
+
     public static void main(String[] args) throws IOException, ParseException {
-
         String file = Paths.get("src", "main", "resources", "stations.html").toString();
-        Document document = Jsoup.parse(Parser.parseFile(file));
-        Element table = document.select("table").get(3);
-        Elements rows = table.select("tr");
-        rows.stream().skip(1).forEach((row) -> {
-            Elements cols = row.select("td");
-            String stationName = cols.get(1).text();
-            String lineName = cols.get(0).child(1).attr("title");
-            List<String> lineNumbers = cols.get(0).children().eachText();
-            List<String> connectionsLineName = cols.get(0).children().eachAttr("title");
-            List<String> connectionsNumber = cols.get(3).children().eachText();
+        parser.parseInputDataHtmlFile(file);
+        createJsonFile();
+        readJsonAndPrintStations();
+    }
 
-            Parser.parseStation(stationName, lineNumbers, connectionsLineName);
-            Parser.parseLines(lineName, lineNumbers);
-            if (connectionsNumber.size() != 0) {
-                Parser.parseConnections(cols, stationName);
+    static void createJsonFile() throws IOException {
+        metro = new Metro(parser.getLines(), parser.getStations(), parser.getConnection());
+        try (FileWriter file = new FileWriter(fileName)) {
+            file.write(GSON.toJson(metro));
+        }
+    }
+
+    static void readJsonAndPrintStations() throws ParseException {
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(parser.parseFile(fileName));
+        Map<String, List<String>> stations = (Map<String, List<String>>) jsonObject.get("stations");
+        checkStations(stations);
+    }
+
+    private static void checkStations(Map<String, List<String>> stations) {
+        for (String lineId : stations.keySet()) {
+            JSONArray stationsArray = (JSONArray) stations.get(lineId);
+            for (Line line : metro.getLines()) {
+                printLineDetails(lineId, stationsArray, line);
             }
-        });
+        }
+    }
 
-        Parser.createJsonFile();
-        Parser.jsonLinesParser();
+    private static void printLineDetails(String lineId, JSONArray stationsArray, Line line) {
+        if (line.getId().equals(lineId)) {
+            System.out.println("Линия " + lineId + " " + line.getName()
+                    + " -> количество станций: " + stationsArray.size());
+        }
     }
 }

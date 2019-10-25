b9c0163fd8e0c67587be7c3d5820f11b44ec087d
Aleksey Vasin <Aleksey_Vasin@epam.com>
2019-10-25 11:22:39 +0300

    refactoring imageResizer

--- module11/11.4/src/main/java/ImageResizer.java
+++ module11/11.4/src/main/java/ImageResizer.java
@@ -1,14 +1,33 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ImageResizer {
 
-    public void resize(File[] files, String dstFolder) {
+    private List<File[]> splitFiles(File[] files) {
+        int middle = files.length / 2;
+        File[] firstArray = new File[middle];
+        System.arraycopy(files, 0, firstArray, 0, firstArray.length);
+        File[] secondArray = new File[files.length - middle];
+        System.arraycopy(files, 0, secondArray, 0, secondArray.length);
+        List<File[]> arrays = Arrays.asList(firstArray, secondArray);
+        return arrays;
+    }
+
+    public void resizeWithExecutor(File[] files, String dstFolder) throws InterruptedException {
         long start = System.currentTimeMillis();
-        splitFiles(files).stream().forEach(e -> {
-            new Thread(() -> {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        splitFiles(files).forEach(e -> {
+            service.submit(() -> {
                 try {
                     for (File file : files) {
                         BufferedImage image = ImageIO.read(file);
@@ -33,68 +52,16 @@ public class ImageResizer {
                                 newImage.setRGB(x, y, rgb);
                             }
                         }
-
-                        File newFile = new File(dstFolder + "/" + file.getName());
-                        ImageIO.write(newImage, "jpg", newFile);
+                        Path newFile = Paths.get(dstFolder, file.getName());
+                        ImageIO.write(newImage, "jpg", newFile.toFile());
                     }
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
-            }).run();
+            });
         });
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
-    }
-
-    private ArrayList<File[]> splitFiles(File[] files) {
-        int middle = files.length / 2;
-
-        File[] firstArray = new File[middle];
-        System.arraycopy(files, 0, firstArray, 0, firstArray.length);
-
-        File[] secondArray = new File[files.length - middle];
-        System.arraycopy(files, 0, secondArray, 0, secondArray.length);
-
-        ArrayList<File[]> arrays = new ArrayList<>();
-        arrays.add(firstArray);
-        arrays.add(secondArray);
-        return arrays;
-    }
-
-    public void resizeWithParallelStream(File[] files, String dstFolder) {
-        long start = System.currentTimeMillis();
-        splitFiles(files).parallelStream().forEach(e -> {
-            try {
-                for (File file : files) {
-                    BufferedImage image = ImageIO.read(file);
-                    if (image == null) {
-                        continue;
-                    }
-
-                    int newWidth = 300;
-                    int newHeight = (int) Math.round(
-                            image.getHeight() / (image.getWidth() / (double) newWidth)
-                    );
-                    BufferedImage newImage = new BufferedImage(
-                            newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                    );
-
-                    int widthStep = image.getWidth() / newWidth;
-                    int heightStep = image.getHeight() / newHeight;
-
-                    for (int x = 0; x < newWidth; x++) {
-                        for (int y = 0; y < newHeight; y++) {
-                            int rgb = image.getRGB(x * widthStep, y * heightStep);
-                            newImage.setRGB(x, y, rgb);
-                        }
-                    }
-
-                    File newFile = new File(dstFolder + "/" + file.getName());
-                    ImageIO.write(newImage, "jpg", newFile);
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
+        service.shutdown();
+        service.awaitTermination(40, TimeUnit.SECONDS);
         System.out.println("Duration: " + (System.currentTimeMillis() - start));
     }
 }
--- module11/11.4/src/main/java/Main.java
+++ module11/11.4/src/main/java/Main.java
@@ -1,15 +1,23 @@
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        String srcFolder = "/users/admin/Desktop/src";
-        String dstFolder = "/users/admin/Desktop/dst";
 
-        File srcDir = new File(srcFolder);
-        File[] files = srcDir.listFiles();
+        String srcFolder = "Users/admin/Desktop/from";
+        String dstFolder = "Users/admin/Desktop/to";
 
-        ImageResizer imageResizer = new ImageResizer();
-        imageResizer.resize(files, dstFolder);
-        imageResizer.resizeWithParallelStream(files, dstFolder);
+        Path pathFrom = Paths.get(srcFolder);
+        Path pathTo = Paths.get(dstFolder);
+
+        File srcDir = new File(pathFrom.toUri());
+        try {
+            File[] files = srcDir.listFiles();
+            ImageResizer imageResizer = new ImageResizer();
+            imageResizer.resizeWithExecutor(files, pathTo.toString());
+        } catch (NullPointerException | InterruptedException e) {
+            System.out.println("files not found");
+        }
     }
 }

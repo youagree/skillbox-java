3a14ed027f0452477c83b08610ce6b67e62a3d9f
Aleksey Vasin <Aleksey_Vasin@epam.com>
2019-12-09 19:26:11 +0300

    add refactor

--- module13/13.5/src/main/java/second_exercise/ProducerWithPromote.java
+++ module13/13.5/src/main/java/second_exercise/ProducerWithPromote.java
@@ -3,16 +3,26 @@ package second_exercise;
 import static second_exercise.utils.Constants.HOST;
 import static second_exercise.utils.Constants.KEY;
 import static second_exercise.utils.Constants.PORT;
+import static second_exercise.utils.Utils.waitSomeTime;
 
 import redis.clients.jedis.Jedis;
 
 public class ProducerWithPromote
 {
     public static void main(String[] args) {
+        int count = 0;
         try (Jedis client = new Jedis(HOST, PORT)) {
             while (true) {
-                String deletedUser = client.lpop(KEY);
-                client.lpush(KEY, deletedUser);
+                waitSomeTime(1000);
+                count++;
+                if (count == 5) {
+                    long randomPosition = (long) (0 + 20 * Math.random());
+                    String user = client.lindex(KEY, randomPosition);
+                    client.lrem(KEY,0, user);
+                    client.lpush(KEY, user);
+                    count = 0;
+                }
+                client.rpoplpush(KEY, KEY);;
             }
         }
     }
